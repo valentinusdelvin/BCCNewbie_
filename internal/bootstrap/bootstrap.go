83be020da813e0f67802600cf8d4877076b53f05
naylasyitha <naylasyitha30@gmail.com>
2025-04-26 21:58:50 +0700

    fixing error

--- internal/bootstrap/bootstrap.go
+++ internal/bootstrap/bootstrap.go
@@ -2,9 +2,12 @@ package bootstrap
 
 import (
 	"fmt"
-	"hackfest-uc/internal/app/user/interface/rest"
-	"hackfest-uc/internal/app/user/repository"
-	"hackfest-uc/internal/app/user/usecase"
+	UserHandler "hackfest-uc/internal/app/user/interface/rest"
+	UserRepo "hackfest-uc/internal/app/user/repository"
+	UserUsecase "hackfest-uc/internal/app/user/usecase"
+	WasteDepositHandler "hackfest-uc/internal/app/waste_deposit/interface/rest"
+	WasteDepositRepo "hackfest-uc/internal/app/waste_deposit/repository"
+	WasteDepositUsecase "hackfest-uc/internal/app/waste_deposit/usecase"
 	"hackfest-uc/internal/domain/entity"
 	"hackfest-uc/internal/infra/env"
 	"hackfest-uc/internal/infra/jwt"
@@ -25,7 +28,7 @@ func Start() error {
 	}
 
 	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		"%s%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		config.DBUsername,
 		config.DBPassword,
 		config.DBHost,
@@ -43,6 +46,11 @@ func Start() error {
 		log.Fatalf("Failed to migrate database: %v", err)
 	}
 
+	err = database.AutoMigrate(entity.WasteDeposit{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
 	app := fiber.New()
 
 	validator := validation.NewInputValidation()
@@ -53,9 +61,13 @@ func Start() error {
 
 	v1 := app.Group("/api/v1")
 
-	userRepo := repository.NewUserMySQL(database)
-	userUsecase := usecase.NewUserUsecase(userRepo, *jwt, *validator)
-	rest.NewUserHandler(v1, *validator, userUsecase, middlewareService)
+	userRepo := UserRepo.NewUserMySQL(database)
+	userUsecase := UserUsecase.NewUserUsecase(userRepo, *jwt, *validator)
+	UserHandler.NewUserHandler(v1, *validator, userUsecase, middlewareService)
+
+	wasteDepositRepo := WasteDepositRepo.NewWasteDepositMySQL(database)
+	wasteDepositUsecase := WasteDepositUsecase.NewWasteDepositUsecase(wasteDepositRepo)
+	WasteDepositHandler.NewWasteDepositHandler(v1, wasteDepositUsecase, middlewareService)
 
 	return app.Listen(fmt.Sprintf(":%d", config.AppPort))
 }
--- internal/infra/env/env.go
+++ internal/infra/env/env.go
@@ -10,11 +10,11 @@ import (
 type Env struct {
 	AppPort int `env:"APP_PORT"`
 
-	DBUsername string `env:"MYSQL_USER"`
-	DBPassword string `env:"MYSQL_PASSWORD"`
-	DBHost     string `env:"MYSQL_HOST"`
-	DBPort     int    `env:"MYSQL_PORT"`
-	DBName     string `env:"MYSQL_DATABASE"`
+	DBUsername string `env:"DB_USER"`
+	DBPassword string `env:"DB_PASSWORD"`
+	DBHost     string `env:"DB_HOST"`
+	DBPort     int    `env:"DB_PORT"`
+	DBName     string `env:"DB_DATABASE"`
 
 	JwtSecret  string `env:"JWT_SECRET"`
 	JwtExpired int    `env:"JWT_EXPIRED"`
