1f5fc551103a4085fdc570d8f2d224164238345e
naylasyitha <naylasyitha30@gmail.com>
2025-04-26 11:28:12 +0700

    register

--- internal/domain/dto/user.go
+++ internal/domain/dto/user.go
@@ -1,1 +1,12 @@
 package dto
+
+type Register struct {
+	Email    string `json:"email" validate:"required,email"`
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type Login struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required,min=8"`
+}
